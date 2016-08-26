d41e873dc52edbc0729a2afb3168178e8026f583
Jose Ines Cantu Arrambide <joseines@ponderosa.com.mx>
2016-08-26 10:22:21 -0500

    Initial Commit

---
+++ KeyChainWrapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KeyChainWrapper
+//
+//  Created by Jose Ines Cantu Arrambide on 8/26/16.
+//  Copyright © 2016 Jose Ines Cantu Arrambide. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
